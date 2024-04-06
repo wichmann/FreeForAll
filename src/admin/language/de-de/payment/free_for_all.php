72e78bc47b07a2c8d2a1a59fe7206cf8a2fbe578
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-04-06 19:49:42 +0200

    Add german translation.

---
+++ src/admin/language/de-de/payment/free_for_all.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title'] = 'FreeForAll';
+
+// Text
+$_['text_extension'] = 'Erweiterung';
+$_['text_success'] = 'Erfolg: Sie haben die Einstellungen von FreeForAll erfolgreich geändert!';
+$_['text_edit'] = 'Free For All bearbeiten';
+
+// Entry
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission'] = 'Warnung: Sie haben keine Berechtigung, diese Einstellungen zu ändern!';
