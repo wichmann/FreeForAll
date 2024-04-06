e4051cbadf8fc8666ea8e549f7ef3dc40f4c34a0
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-04-06 17:19:46 +0200

    Cleanup catalog source files.

--- src/catalog/language/en-gb/payment/free_for_all.php
+++ src/catalog/language/en-gb/payment/free_for_all.php
@@ -1,11 +1,3 @@
 <?php
 // Heading
-$_['heading_title']      = 'FreeForAll';
-
-// Text
-$_['text_title']         = 'Please enter your card details below';
-$_['text_description']   = 'This payment method is a dummy one and will not store credit card information nor should it be used on a live site.';
-$_['text_card_use']      = 'Use credit card';
-$_['text_month']         = 'Months';
-$_['text_year']          = 'Years';
-$_['text_store']         = 'Remember My Card';
+$_['heading_title'] = 'FreeForAll';
--- src/catalog/model/payment/free_for_all.php
+++ src/catalog/model/payment/free_for_all.php
@@ -6,13 +6,15 @@ namespace Opencart\Catalog\Model\Extension\FreeForAll\Payment;
  *
  * @package
  */
-class FreeForAll extends \Opencart\System\Engine\Model {
+class FreeForAll extends \Opencart\System\Engine\Model
+{
 	/**
 	 * @param array $address
 	 *
 	 * @return array
 	 */
-	public function getMethods(array $address = []): array {
+	public function getMethods(array $address = []): array
+	{
 		$this->load->language('extension/free_for_all/payment/free_for_all');
 
 		$option_data['free_for_all'] = [
@@ -21,9 +23,9 @@ class FreeForAll extends \Opencart\System\Engine\Model {
 		];
 
 		$method_data = [
-			'code'       => 'free_for_all',
-			'name'       => $this->language->get('heading_title'),
-			'option'     => $option_data,
+			'code' => 'free_for_all',
+			'name' => $this->language->get('heading_title'),
+			'option' => $option_data,
 			'sort_order' => $this->config->get('payment_free_for_all_sort_order')
 		];
 
